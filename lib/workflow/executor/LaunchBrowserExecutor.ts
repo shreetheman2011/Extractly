192f3c83e3b05af4377638f0b8b2c4d12e7102b7
teamncna <teamncna@gmail.com>
2025-12-08 18:47:38 -0800

    changes

--- lib/workflow/executor/LaunchBrowserExecutor.ts
+++ lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -10,7 +10,6 @@ export async function LaunchBrowserExecutor(
     const websiteUrl = environment.getInput("Website URL");
     const browser = await puppeteer.launch({
       headless: true,
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
