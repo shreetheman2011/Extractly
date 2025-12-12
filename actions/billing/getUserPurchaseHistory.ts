28878598b85cf9659cd930493b04484ac78a63d0
shreetheman2011 <154644066+shreetheman2011@users.noreply.github.com>
2025-12-11 19:20:59 -0800

    Add function to get user purchase history

---
+++ actions/billing/getUserPurchaseHistory.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export async function GetUserPurchaseHistory() {
+  const { userId } = auth();
+  if (!userId) throw new Error("User not authenticated");
+
+  return prisma.userPurchase.findMany({
+    where: { userId },
+    orderBy: {
+      date: "asc",
+    },
+  });
+}
